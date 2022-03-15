22e1220529d5a0cc234c871d5b445fee03c6d517
Laura <lauraorwako@gmail.com>
2022-03-15 22:13:34 +0300

    First commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const menuBtn = document.querySelector('.menubutton');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+    if(!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+    }
+});
